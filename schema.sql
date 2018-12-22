86d0abebd7d0ada968fcf807289fcd662272dbaf
David Ngo <ngo.david.huy@gmail.com>
2018-12-21 19:58:02 -0800

    Implements seeder.js and controllers to handle GET requests

--- schema.sql
+++ schema.sql
@@ -6,50 +6,34 @@ USE `yelp`;
 
 CREATE TABLE `users` (
   `_id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  `username` VARCHAR(15),
-  UNIQUE KEY (username)
+  `username` VARCHAR(30),
+  UNIQUE KEY (_id, username)
 );
 
 CREATE TABLE `restaurants` (
   `_id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   `rest_name` VARCHAR(50),
-  `street_number` VARCHAR(4) NOT NULL,
-  `street_name` VARCHAR(50) NOT NULL,
-  `street_type` VARCHAR(5) NOT NULL,
-  `city` VARCHAR(20) NOT NULL,
-  `state` VARCHAR(2) NOT NULL,
-  `zip` VARCHAR(5) NOT NULL,
   `days_open` JSON NOT NULL,
   `opening_time` TIME NOT NULL,
   `closing_time` TIME NOT NULL,
-  `table_count` INT NOT NULL
+  `price_range` ENUM('Under $10', '$11-30', '$31-60', 'Above $61') NOT NULL,
+  `health_score` INT NOT NULL
 );
 
-CREATE TABLE tables (
+CREATE TABLE `reservations` (
   `_id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   `rest_id` INT NOT NULL,
-  `min_seating` INT NOT NULL,
-  `max_seating` INT NOT NULL,
-  `availability` JSON NOT NULL,
-  FOREIGN KEY (rest_id)
-    REFERENCES restaurants(_id)
-);
-
-CREATE TABLE timeslots (
-  `_id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  `rest_id` INT NOT NULL,
-  `table_id` INT NOT NULL,
   `user_id` INT NOT NULL,
-  `date` DATE NOT NULL,
+  `day` DATE NOT NULL,
+  `party_size` INT NOT NULL,
   `start_time` TIME NOT NULL,
   `end_time` TIME NOT NULL,
-  `party_size` INT NOT NULL,
   FOREIGN KEY (rest_id)
     REFERENCES restaurants(_id),
-  FOREIGN KEY (table_id)
-    REFERENCES tables(_id),
   FOREIGN KEY (user_id)
     REFERENCES users(_id)
 );
+
+
 
 
