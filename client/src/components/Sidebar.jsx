3c050bfca74665843242c0d440390807f2210070
David Ngo <ngo.david.huy@gmail.com>
2018-12-29 21:26:51 -0800

    Pushes latest branch for review

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Reservations from './reservations/Reservations.jsx';
 import Hours from './hours/Hours.jsx';
 import Summary from './summary/Summary.jsx';
 
-const Sidebar = () => (
-    <div id="sidebar">
-      <Reservations />
-      <Summary />
-      <Hours />
-    </div>
-  );
+class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+    fetch('/restaurant/:rest_id').then(response => {
+      return response.json();
+    }).then(results => {
+      console.log(results);
+    })
+  }
+
+  render() {
+    return (
+      <div id="sidebar">
+        <Reservations />
+        <Summary />
+        <Hours />
+      </div>
+    );
+  }
+}
 
 export default Sidebar;
