8bfd7da3c2bc67c09c4db21902f8980efa915d91
esey <esey.iyasu@gmail.com>
2018-12-29 21:55:26 -0800

    you would make something like this

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,5 +3,8 @@ import ReactDOM from 'react-dom';
 import Sidebar from './components/Sidebar.jsx';
 
 
-
-ReactDOM.render(<Sidebar />, document.getElementById('sidebar'));
+//this is just a demo 
+const rest_id = window.rest_id;
+//to verify we are getting a dynameic rest_id
+console.log("this is what you put in your address bar" + rest_id);
+ReactDOM.render(<Sidebar rest_id={rest_id} />, document.getElementById('sidebar'));
