4be9032c4cfb28e954bb07252098f6f52ad7a31c
David Ngo <ngo.david.huy@gmail.com>
2019-01-03 15:10:16 -0800

    Attempts normalize.css file and changes icon.svg filename

--- client/src/components/icons/Icon.jsx
+++ client/src/components/icons/Icon.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './icons.svg';
+import './sidebar-icons.svg';
 
 const Icon = ({name, width, height, fill}) => (
   <svg width={width} height={height} fill={fill || '#000'}>
-    <use xlinkHref={`#icons_${name}`} />
+    <use xlinkHref={`#sidebar-icons_${name}`} />
   </svg>
 )
 
